dcb09c5dcab84406dbe3a7cceb1f16bbe734a50c
Henrik Haug <henrik.h.haug@gmail.com>
2025-09-23 16:19:52 +0200

    small change

--- project1/main_gd.py
+++ project1/main_gd.py
@@ -7,12 +7,11 @@ from src.regression import RegressionAnalysis
 from src.utils import polynomial_features, scale_data, runge
 
 
-N = 100         # sample size
-degree = 8      # polynomial degree
-lam = 1e-2      # Ridge lambda
-eta = 1e-2      # learning rate for GD
+N = 100  # sample size
+degree = 8  # polynomial degree
+lam = 1e-2  # Ridge lambda
+eta = 1e-2  # learning rate for GD
 num_iters = 10000
-
 
 
 x = np.linspace(-1, 1, N)
@@ -30,7 +29,9 @@ X_train, X_test, y_train, y_test, x_train, x_test = train_test_split(
 data = [X_train, X_test, y_train, y_test, x_train, x_test, y_mean]
 
 
-analysis = RegressionAnalysis(data, degree=degree, lam=lam, eta=eta, num_iters=num_iters)
+analysis = RegressionAnalysis(
+    data, degree=degree, lam=lam, eta=eta, num_iters=num_iters
+)
 analysis.fit_analytical()
 analysis.fit_gradient_descent()
 analysis.predict()
@@ -38,7 +39,9 @@ analysis.calculate_metrics()
 
 
 X_full = polynomial_features(x, degree)
-X_full_norm, _, _ = scale_data(X_full, y_true)  # normalize consistently with full dataset
+X_full_norm, _, _ = scale_data(
+    X_full, y_true
+)  # normalize consistently with full dataset
 
 y_pred_ols_analytical = X_full_norm @ analysis.theta_ols_analytical + y_mean
 y_pred_ols_gd = X_full_norm @ analysis.theta_ols_gd + y_mean
@@ -46,8 +49,12 @@ y_pred_ridge_analytical = X_full_norm @ analysis.theta_ridge_analytical + y_mean
 y_pred_ridge_gd = X_full_norm @ analysis.theta_ridge_gd + y_mean
 
 
+solutions = [
+    y_pred_ols_analytical,
+    y_pred_ols_gd,
+    y_pred_ridge_analytical,
+    y_pred_ridge_gd,
+    y_true,
+]
 
-
-solutions = [y_pred_ols_analytical, y_pred_ols_gd, y_pred_ridge_analytical, y_pred_ridge_gd, y_true]
-
-solution_comparison(x, solutions=solutions,sample_size=N, degree=degree, lam=lam)
+solution_comparison(x, solutions=solutions, sample_size=N, degree=degree, lam=lam)
