b6f7a26ddc1f5a1efbf1f9c323e2b65c6fb3eb9b
livelst <livelst@uio.no>
2025-09-25 22:26:21 +0200

    main_gd is now updated to scale the train data first, and then scale test data based on train scale parameters, and plotting is working

--- project1/main_gd.py
+++ project1/main_gd.py
@@ -6,54 +6,87 @@ from src.regression import RegressionAnalysis
 from src.utils import polynomial_features, scale_data, runge
 
 
-N = 100  # sample size
-degree = 8  # polynomial degree
-lam = 1e-2  # Ridge lambda
-eta = 1e-2  # learning rate for GD
+N = 500
+degree = 8  
+lam = 1e-2  
+eta = 1e-2  
 num_iters = 10000
 
 
 x = np.linspace(-1, 1, N)
 np.random.seed(42)
 random_noise = np.random.normal(0, 0.1, N)
-y_true = runge(x) + random_noise
-
-X = polynomial_features(x, degree)
-X_norm, y_centered, y_mean = scale_data(X, y_true)
-
-X_train, X_test, y_train, y_test, x_train, x_test = train_test_split(
-    X_norm, y_centered, x, test_size=0.2, random_state=42
-)
-
-data = [X_train, X_test, y_train, y_test, x_train, x_test, y_mean]
+y_true = runge(x)   
+y_noise = y_true + random_noise                 
 
 
+X = polynomial_features(x, degree)           
+X_norm, y_centered, _, _, y_mean = scale_data(X, y_noise)  
+
+
+# =============================================================================
+#                          FULL DATASET ANALYSIS
+# =============================================================================
+
+data = [X_norm, y_centered, y_mean]
 analysis = RegressionAnalysis(
-    data, degree=degree, lam=lam, eta=eta, num_iters=num_iters
+    data, 
+    degree=degree, 
+    lam=lam, eta=eta, 
+    num_iters=num_iters,
+    full_dataset=True  
 )
-analysis.fit_one('ols', 'analytical')
-analysis.fit_one('ols', 'gd')
-analysis.fit_one('ridge', 'analytical')  
-analysis.fit_one('ridge', 'gd')
 
-
-X_full = polynomial_features(x, degree)
-X_full_norm, _, _ = scale_data(
-    X_full, y_true
-)  # normalize consistently with full dataset
-
-y_pred_ols_analytical = X_full_norm @ analysis.get_theta('ols', 'analytical') + y_mean
-y_pred_ols_gd = X_full_norm @ analysis.get_theta('ols', 'gd') + y_mean
-y_pred_ridge_analytical = X_full_norm @ analysis.get_theta('ridge', 'analytical') + y_mean
-y_pred_ridge_gd = X_full_norm @ analysis.get_theta('ridge', 'gd') + y_mean
-
+analysis.fit_many(models=('ols', 'ridge'), 
+                  opts=('analytical', 'gd'))
 
 solutions = [
-    y_pred_ols_analytical,
-    y_pred_ols_gd,
-    y_pred_ridge_analytical,
-    y_pred_ridge_gd,
-    y_true,
+    analysis.runs[('ols', 'analytical')]['y_pred_test'],
+    analysis.runs[('ols', 'gd')]['y_pred_test'], 
+    analysis.runs[('ridge', 'analytical')]['y_pred_test'],
+    analysis.runs[('ridge', 'gd')]['y_pred_test'],
+    x        
 ]
 
-solution_comparison(x, solutions=solutions, sample_size=N, degree=degree, lam=lam)
+solution_comparison(x, y_noise, y_true, solutions=solutions, sample_size=N, degree=degree, lam=lam, test=False)
+
+
+
+
+
+
+# =============================================================================
+#                        TEST SPLIT ANALYSIS  
+# =============================================================================
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_noise, test_size=0.2, random_state=42)
+x_train = X_train[:, 0] 
+x_test = X_test[:, 0] 
+
+# Scaling the training data and using the same parameters to scale the test data (to avoid data leakage)
+X_train_s, y_train_s, X_mean, X_std, y_mean = scale_data(X_train, y_train)
+X_test_s, y_test_s, _, _, _ = scale_data(X_test, y_test, X_mean, X_std, y_mean)
+
+
+data_test = [X_train_s, X_test_s, y_train_s, y_test_s, x_train, x_test, y_mean]
+analysis_test = RegressionAnalysis(
+    data_test, 
+    degree=degree, 
+    lam=lam, eta=eta, 
+    num_iters=num_iters,
+    full_dataset=False  
+)
+
+analysis_test.fit_many(models=('ols', 'ridge'), 
+                      opts=('analytical', 'gd'))
+
+
+solutions_test = [
+    analysis_test.runs[('ols', 'analytical')]['y_pred_test'],
+    analysis_test.runs[('ols', 'gd')]['y_pred_test'], 
+    analysis_test.runs[('ridge', 'analytical')]['y_pred_test'],
+    analysis_test.runs[('ridge', 'gd')]['y_pred_test'],
+    x_test       
+]
+
+solution_comparison(x, y_noise, y_true, solutions=solutions_test, sample_size=N, degree=degree, lam=lam, test=True)
