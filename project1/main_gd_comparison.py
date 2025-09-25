fixed scaling and plotting for comparing all gradient descent methods, all main files now work (except Lasso - Henrik can continue)

project1/main_gd_comparison.py
@@ -14,14 +14,19 @@ num_iters = 10000
 x = np.linspace(-1, 1, N)
 np.random.seed(42)
 random_noise = np.random.normal(0, 0.1, N)
-y_true = runge(x) + random_noise
-
+y_true = runge(x)
+y_noise = y_true + random_noise
 X = polynomial_features(x, degree)
-X_norm, y_centered, y_mean = scale_data(X, y_true)
 
-# Use full dataset mode
+
+
+# =============================================================================
+#                          FULL DATASET ANALYSIS
+# =============================================================================
+X_norm, y_centered, X_mean, X_std, y_mean = scale_data(X, y_true)
+data = [X_norm, y_centered, y_mean]
 analysis = RegressionAnalysis(
-    [X_norm, y_centered], 
+    data, 
     degree=degree, 
     lam=lam, 
     eta=eta, 
@@ -29,11 +34,10 @@ analysis = RegressionAnalysis(
     full_dataset=True
 )
 
-# Fit all methods
 analysis.fit_many(models=('ols', 'ridge'), 
                   opts=('analytical', 'gd', 'momentum', 'adagrad', 'rmsprop', 'adam'))
 
-# Get predictions - the new class stores these automatically
+
 solutions_ols = [
     analysis.runs[('ols', 'analytical')]['y_pred_test'],
     analysis.runs[('ols', 'gd')]['y_pred_test'], 
@@ -41,7 +45,7 @@ solutions_ols = [
     analysis.runs[('ols', 'adagrad')]['y_pred_test'],
     analysis.runs[('ols', 'rmsprop')]['y_pred_test'],
     analysis.runs[('ols', 'adam')]['y_pred_test'],
-    y_true,
+    x,
 ]
 
 solutions_ridge = [
@@ -51,8 +55,59 @@ solutions_ridge = [
     analysis.runs[('ridge', 'adagrad')]['y_pred_test'],
     analysis.runs[('ridge', 'rmsprop')]['y_pred_test'],
     analysis.runs[('ridge', 'adam')]['y_pred_test'],
-    y_true,
+    x,
 ]
 
-solution_comparison_gd(x, solutions=solutions_ols, sample_size=N, degree=degree, lam=lam)
-solution_comparison_gd(x, solutions=solutions_ridge, sample_size=N, degree=degree, lam=lam)
+solution_comparison_gd(x, y_noise, y_true, solutions=solutions_ols, sample_size=N, degree=degree, lam=lam, test=False)
+solution_comparison_gd(x, y_noise, y_true, solutions=solutions_ridge, sample_size=N, degree=degree, lam=lam, test=False)
+
+
+
+
+
+# =============================================================================
+#                        TEST SPLIT ANALYSIS  
+# =============================================================================
+X_train, X_test, y_train, y_test = train_test_split(X, y_noise, test_size=0.2, random_state=42)
+x_train = X_train[:, 0] 
+x_test = X_test[:, 0] 
+
+# Scaling the training data and using the same parameters to scale the test data (to avoid data leakage)
+X_train_s, y_train_s, X_mean, X_std, y_mean = scale_data(X_train, y_train)
+X_test_s, y_test_s, _, _, _ = scale_data(X_test, y_test, X_mean, X_std, y_mean)
+
+
+data_test = [X_train_s, X_test_s, y_train_s, y_test_s, x_train, x_test, y_mean]
+analysis_test = RegressionAnalysis(
+    data_test, 
+    degree=degree, 
+    lam=lam, eta=eta, 
+    num_iters=num_iters,
+    full_dataset=False  
+)
+
+analysis_test.fit_many(models=('ols', 'ridge'), 
+                      opts=('analytical', 'gd', 'momentum', 'adagrad', 'rmsprop', 'adam'))
+
+solutions_test_ols = [
+    analysis_test.runs[('ols', 'analytical')]['y_pred_test'],
+    analysis_test.runs[('ols', 'gd')]['y_pred_test'], 
+    analysis_test.runs[('ols', 'momentum')]['y_pred_test'],
+    analysis_test.runs[('ols', 'adagrad')]['y_pred_test'],
+    analysis_test.runs[('ols', 'rmsprop')]['y_pred_test'],
+    analysis_test.runs[('ols', 'adam')]['y_pred_test'],
+    x_test,       
+]
+
+solutions_test_ridge = [
+    analysis_test.runs[('ridge', 'analytical')]['y_pred_test'],
+    analysis_test.runs[('ridge', 'gd')]['y_pred_test'],
+    analysis_test.runs[('ridge', 'momentum')]['y_pred_test'], 
+    analysis_test.runs[('ridge', 'adagrad')]['y_pred_test'],
+    analysis_test.runs[('ridge', 'rmsprop')]['y_pred_test'],
+    analysis_test.runs[('ridge', 'adam')]['y_pred_test'],
+    x_test,       
+]
+
+solution_comparison_gd(x, y_noise, y_true, solutions=solutions_test_ols, sample_size=N, degree=degree, lam=lam, test=True)
+solution_comparison_gd(x, y_noise, y_true, solutions=solutions_test_ridge, sample_size=N, degree=degree, lam=lam, test=True)