15cf27be6c7c3158c13d2cd9c94143ef9a7908eb
Henrik Haug <henrik.h.haug@gmail.com>
2025-09-25 11:38:30 +0200

    Edited structure

--- project1/main_ols.py
+++ project1/main_ols.py
@@ -11,7 +11,6 @@ sample_sizes = [500, 1000, 10000]
 all_results = {}
 
 for N in sample_sizes:
-
     x = np.linspace(-1, 1, N)
     y_true = runge(x)
     degrees = range(1, 16)
@@ -21,47 +20,52 @@ for N in sample_sizes:
     for deg in degrees:
         X = polynomial_features(x, deg)
         X_norm, y_centered, y_mean = scale_data(X, y_true)
-        X_train, X_test, y_train, y_test, x_train, x_test = train_test_split(X_norm, y_centered, x, test_size=0.2, random_state=42)
-        
+        X_train, X_test, y_train, y_test, x_train, x_test = train_test_split(
+            X_norm, y_centered, x, test_size=0.2, random_state=42
+        )
+
         data_splits[deg] = [X_train, X_test, y_train, y_test, x_train, x_test, y_mean]
 
     # Create instances for all degrees
     results_current = {
-        'degrees': list(degrees),
-        'train_mse': [],
-        'test_mse': [],
-        'train_r2': [],
-        'test_r2': [],
-        'instances': []
+        "degrees": list(degrees),
+        "train_mse": [],
+        "test_mse": [],
+        "train_r2": [],
+        "test_r2": [],
+        "instances": [],
     }
 
     theta1_evolution = []  # Initialize for all N, but only use for N=1000
 
     for deg in degrees:
-        analysis = RegressionAnalysis(data_splits[deg], degree=deg, lam=None, eta=None, num_iters=None)
-        
+        analysis = RegressionAnalysis(
+            data_splits[deg], degree=deg, lam=None, eta=None, num_iters=None
+        )
+
         analysis.fit_analytical()
         analysis.predict()
         analysis.calculate_metrics()
-        
-        results_current['train_mse'].append(analysis.train_mse_ols_analytical)
-        results_current['test_mse'].append(analysis.mse_ols_analytical)
-        results_current['train_r2'].append(analysis.train_r2_ols_analytical)
-        results_current['test_r2'].append(analysis.r2_ols_analytical)
-        results_current['instances'].append(analysis)
+
+        results_current["train_mse"].append(analysis.train_mse_ols_analytical)
+        results_current["test_mse"].append(analysis.mse_ols_analytical)
+        results_current["train_r2"].append(analysis.train_r2_ols_analytical)
+        results_current["test_r2"].append(analysis.r2_ols_analytical)
+        results_current["instances"].append(analysis)
 
         # Store theta1 evolution for all N (only plot for N=1000)
         theta1 = analysis.theta_ols_analytical[0]
         theta1_evolution.append(theta1)
-        
+
     all_results[N] = results_current
-    
 
     # Further analysis for N=1000
     if N == 1000:
-        mse_degree_ols(all_results, sample_size=1000)                     # MSE for N=1000 (a)
-        r2_degree_ols(results_current, sample_size=1000)                  # R2 for N=1000 (a) 
-        theta_evolution_ols(degrees, theta1_evolution, sample_size=1000)  # Evolution of theta1 for N=1000 (a)
+        mse_degree_ols(all_results, sample_size=1000)  # MSE for N=1000 (a)
+        r2_degree_ols(results_current, sample_size=1000)  # R2 for N=1000 (a)
+        theta_evolution_ols(
+            degrees, theta1_evolution, sample_size=1000
+        )  # Evolution of theta1 for N=1000 (a)
 
 # Plot for multiple sample sizes
 mse_degree_multiple(all_results, sample_sizes)
