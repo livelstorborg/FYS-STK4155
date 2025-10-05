1cf3dc58194d36ea9e4b7ac550ae556560c7e7cc
livelst <livelst@uio.no>
2025-10-05 15:42:03 +0200

    added different etas for table for ridge optimizers

--- project1/main_gd.py
+++ project1/main_gd.py
@@ -34,13 +34,17 @@ X_test_s, y_test_s, _, _, _ = scale_data(X_test, y_test, X_mean, X_std, y_mean)
 data = [X_train_s, X_test_s, y_train_s, y_test_s, x_train, x_test, y_mean]
 
 
-eta_list_gd = [0.37, 0.365, 0.2, 0.1]
-eta_list_momentum = [0.4, 0.3, 0.1, 0.05]
-eta_list_adagrad = [0.3, 0.25, 0.15, 0.1]
-eta_list_rmsprop = [0.02, 0.01, 0.005, 0.001]
-eta_list_adam = [0.2, 0.1, 0.05, 0.02]
+eta_list_gd_ols = [0.37, 0.365, 0.2, 0.1]
+eta_list_momentum_ols = [0.4, 0.3, 0.1, 0.05]
+eta_list_adagrad_ols = [0.3, 0.25, 0.15, 0.1]
+eta_list_rmsprop_ols = [0.02, 0.01, 0.005, 0.001]
+eta_list_adam_ols = [0.2, 0.1, 0.05, 0.02]
 
-
+eta_list_gd_ridge = [0.37, 0.365, 0.2, 0.1]
+eta_list_momentum_ridge = [0.22, 0.2, 0.15, 0.05]
+eta_list_adagrad_ridge = [0.225, 0.22, 0.215, 0.15]
+eta_list_rmsprop_ridge = [0.01, 0.005, 0.002, 0.001]
+eta_list_adam_ridge = [0.25, 0.2, 0.1, 0.05, 0.02]
 
 
 
@@ -52,12 +56,20 @@ eta_list_adam = [0.2, 0.1, 0.05, 0.02]
 # ============================================================================
 
 
-optimizers = {
-    'gd': eta_list_gd,
-    'momentum': eta_list_momentum,
-    'adagrad': eta_list_adagrad,
-    'rmsprop': eta_list_rmsprop,
-    'adam': eta_list_adam
+optimizers_ols = {
+    'gd': eta_list_gd_ols,
+    'momentum': eta_list_momentum_ols,
+    'adagrad': eta_list_adagrad_ols,
+    'rmsprop': eta_list_rmsprop_ols,
+    'adam': eta_list_adam_ols
+}
+
+optimizers_ridge = {
+    'gd': eta_list_gd_ridge,
+    'momentum': eta_list_momentum_ridge,
+    'adagrad': eta_list_adagrad_ridge,
+    'rmsprop': eta_list_rmsprop_ridge,
+    'adam': eta_list_adam_ridge
 }
 
 
@@ -65,7 +77,8 @@ mse_history_ols_gd, mse_history_ols_momentum, mse_history_ols_adagrad, mse_histo
 mse_history_ridge_gd, mse_history_ridge_momentum, mse_history_ridge_adagrad, mse_history_ridge_rmsprop, mse_history_ridge_adam = [], [], [], [], []
 
 
-for opt_name, eta_list in optimizers.items():
+# Run OLS with OLS-specific eta values
+for opt_name, eta_list in optimizers_ols.items():
     for eta in eta_list:
         analysis = RegressionAnalysis(
             data,
@@ -76,23 +89,44 @@ for opt_name, eta_list in optimizers.items():
             full_dataset=False,
             tol_relative=1e-5
         )
-        analysis.fit(models=('ols', 'ridge'), opts=opt_name)
+        analysis.fit(models=('ols',), opts=opt_name)
 
-        # Store MSE histories for each model and optimizer
+        # Store MSE histories for OLS
         if opt_name == 'gd':
             mse_history_ols_gd.append(analysis.runs[('ols', opt_name)]['history'])
-            mse_history_ridge_gd.append(analysis.runs[('ridge', opt_name)]['history'])
         elif opt_name == 'momentum':
             mse_history_ols_momentum.append(analysis.runs[('ols', opt_name)]['history'])
-            mse_history_ridge_momentum.append(analysis.runs[('ridge', opt_name)]['history'])
         elif opt_name == 'adagrad':
             mse_history_ols_adagrad.append(analysis.runs[('ols', opt_name)]['history'])
-            mse_history_ridge_adagrad.append(analysis.runs[('ridge', opt_name)]['history'])
         elif opt_name == 'rmsprop':
             mse_history_ols_rmsprop.append(analysis.runs[('ols', opt_name)]['history'])
-            mse_history_ridge_rmsprop.append(analysis.runs[('ridge', opt_name)]['history'])
         elif opt_name == 'adam':
             mse_history_ols_adam.append(analysis.runs[('ols', opt_name)]['history'])
+
+# Run Ridge with Ridge-specific eta values
+for opt_name, eta_list in optimizers_ridge.items():
+    for eta in eta_list:
+        analysis = RegressionAnalysis(
+            data,
+            degree=degree,
+            lam=lam,
+            eta=eta,
+            num_iters=num_iters,
+            full_dataset=False,
+            tol_relative=1e-5
+        )
+        analysis.fit(models=('ridge',), opts=opt_name)
+
+        # Store MSE histories for Ridge
+        if opt_name == 'gd':
+            mse_history_ridge_gd.append(analysis.runs[('ridge', opt_name)]['history'])
+        elif opt_name == 'momentum':
+            mse_history_ridge_momentum.append(analysis.runs[('ridge', opt_name)]['history'])
+        elif opt_name == 'adagrad':
+            mse_history_ridge_adagrad.append(analysis.runs[('ridge', opt_name)]['history'])
+        elif opt_name == 'rmsprop':
+            mse_history_ridge_rmsprop.append(analysis.runs[('ridge', opt_name)]['history'])
+        elif opt_name == 'adam':
             mse_history_ridge_adam.append(analysis.runs[('ridge', opt_name)]['history'])
 
 
@@ -111,10 +145,10 @@ for opt_name, eta_list in optimizers.items():
 
 fig, ax = plt.subplots(figsize=(10, 6))
 
-n_etas = len(eta_list_gd)
+n_etas = len(eta_list_gd_ols)
 colors = cm.plasma(np.linspace(0.15, 0.95, n_etas))
 
-for i, eta in enumerate(eta_list_gd):
+for i, eta in enumerate(eta_list_gd_ols):
     ax.plot(range(1, len(mse_history_ols_gd[i]) + 1), mse_history_ols_gd[i], '-', 
             color=colors[i], linewidth=2.5, label=f'OLS (η={eta})')
     
@@ -146,7 +180,7 @@ ax.legend(fontsize=16, loc='best', ncol=2)
 
 plt.tight_layout()
 plt.savefig('figs/gd_ols_vs_ridge.pdf', dpi=300, bbox_inches='tight')
-plt.show()
+# plt.show()
 
 
 
@@ -159,18 +193,18 @@ plt.show()
 # ============================================================================
 
 methods_data = [
-    ('GD', eta_list_gd, mse_history_ols_gd, mse_history_ridge_gd),
-    ('Momentum', eta_list_momentum, mse_history_ols_momentum, mse_history_ridge_momentum),
-    ('AdaGrad', eta_list_adagrad, mse_history_ols_adagrad, mse_history_ridge_adagrad),
-    ('RMSprop', eta_list_rmsprop, mse_history_ols_rmsprop, mse_history_ridge_rmsprop),
-    ('Adam', eta_list_adam, mse_history_ols_adam, mse_history_ridge_adam)
+    ('GD', eta_list_gd_ols, eta_list_gd_ridge, mse_history_ols_gd, mse_history_ridge_gd),
+    ('Momentum', eta_list_momentum_ols, eta_list_momentum_ridge, mse_history_ols_momentum, mse_history_ridge_momentum),
+    ('AdaGrad', eta_list_adagrad_ols, eta_list_adagrad_ridge, mse_history_ols_adagrad, mse_history_ridge_adagrad),
+    ('RMSprop', eta_list_rmsprop_ols, eta_list_rmsprop_ridge, mse_history_ols_rmsprop, mse_history_ridge_rmsprop),
+    ('Adam', eta_list_adam_ols, eta_list_adam_ridge, mse_history_ols_adam, mse_history_ridge_adam)
 ]
 
 
 # ----- OLS -----
 ols_data = []
-for method_name, etas, hist_ols, _ in methods_data:
-    for i, eta in enumerate(etas):
+for method_name, etas_ols, etas_ridge, hist_ols, hist_ridge in methods_data:
+    for i, eta in enumerate(etas_ols):
         iterations = len(hist_ols[i])
         final_mse = hist_ols[i][-1]
         initial_mse = hist_ols[i][0]
@@ -199,8 +233,8 @@ df_ols = pd.DataFrame(ols_data)
 
 # ----- RIDGE -----
 ridge_data = []
-for method_name, etas, _, hist_ridge in methods_data:
-    for i, eta in enumerate(etas):
+for method_name, etas_ols, etas_ridge, hist_ols, hist_ridge in methods_data:
+    for i, eta in enumerate(etas_ridge):
         iterations = len(hist_ridge[i])
         final_mse = hist_ridge[i][-1]
         initial_mse = hist_ridge[i][0]
